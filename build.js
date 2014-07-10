8cd9cf246bb3c6c0f2b3574b7a970608a6786404
Cyril Fluck <github@fluck.fr>
2014-07-09 20:09:18 -0700

    Reproduce issue

---
+++ build.js
@@ -0,0 +1,10 @@
+{
+    baseUrl: ".",
+    paths: {
+        "oasis": "bower_components/oasis.js/dist/oasis.amd"
+    },
+    name: "main",
+    include: "main",
+    out: "main.min.js",
+    wrap: true  
+}
---
+++ main.js
@@ -0,0 +1,2 @@
+define(["oasis"], function(o) {
+});
